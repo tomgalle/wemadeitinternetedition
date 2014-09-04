d2096e495088a73502d0eb79c51a2a28063cc1cb
tomgalle <admin@TOMGALLECOM.local>
2014-09-04 18:31:49 -0400

    music added

--- js/script.js
+++ js/script.js
@@ -1,97 +1,109 @@
 $(document).ready(function() {
 
-	 
-	// // SOUNDCLOUD WIDGET
+	// SOUNDCLOUD WIDGET
 
-	// var widget = SC.Widget(document.getElementById('soundcloud_widget'));
-	//         widget.bind(SC.Widget.Events.READY, function() {
-	//         console.log('Ready...');
-	//     });
+	var widget = SC.Widget(document.getElementById('soundcloud_widget'));
 
-	//     $('button').click(function() {
-	//         widget.toggle();
-	//     });
-	// });
-       
+	widget.bind(SC.Widget.Events.READY,function(){ //When player is ready, attach an event
+	    widget.bind(SC.Widget.Events.PLAY,function(){ //When song starts playing
+	        widget.seekTo(14320);//90 seconds from the start = 1:30 min
+	        widget.unbind(SC.Widget.Events.PLAY); //Remove event, to avoid problems when playing is paused and then restarted
+	        setTimeout(function(){init();}, 1000);
 
-
-	console.log('lol');
-    var dollar01 = $('.dollar');
-	var dollar02 = dollar01.clone();
-	var dollars = []
-	var pageWith = $(window).width();
+	    });
+	});
 	
-
-	for ( var i = 0; i < 10; i++ ) {
-
-		var dollar = dollar01.clone();
-		dollars.push(dollar);
-		var randomX = Math.floor(Math.random() * pageWith );
-		var yspeed = Math.random() * 8 + 1;
-
-		dollar.css({
-			left: randomX
-		})
+    $('button').click(function() {
+        widget.toggle();
+        // widget.play();
+    });
 
 
 
-		$(document.body).append(dollar);
-		drop(dollar, yspeed);
+	function init(){
+
+	    console.log('lol');
+	    var dollar01 = $('.dollar');
+		var dollars = [];
+		var pageWith = $(window).width();
+		
+
+		for ( var i = 0; i < 10; i++ ) {
+
+			var dollar = dollar01.clone();
+			dollars.push(dollar);
+			var randomX = Math.floor(Math.random() * pageWith );
+			var yspeed = Math.random() * 7 + 1;
+
+			dollar.css({
+				left: randomX
+			})
 
 
-	}
-	console.log(dollar);
+			$(document.body).append(dollar);
+			drop(dollar, yspeed);
+
+
+		}
+		console.log(dollar);
+
+
+
+	    // DROP 1 FUNCTION
+
+		function drop(bill, yspeed) {
+
+		    var currPos = bill.offset();
+		    var leftPos = currPos.left;
+		    var topPos = currPos.top;
+		    var pageHeight = $(window).height();
+		    var pageWith = $(window).width();
+		    var randomX = Math.floor(Math.random() * pageWith );
+
+		    console.log(randomX);               
+		   
+		    
+		    var zigzag = function()  {
+
+		        console.log('zigzag');
+		        
+		        bill.transition({ x: leftPos + 60, onComplete: function(){
+		            bill.transition({x: leftPos - 60});
+		            zigzag();
+		            }
+		        });
+		    }
+
+		    zigzag();
+
+		    var ymovement = function() {
+
+		        var currPos2 = bill.offset().top;
+		        if ( currPos2 > pageHeight ) {
+		        
+		            var pageWith = $(window).width();
+		            var randomX = Math.floor(Math.random() * pageWith );
+		        
+		            bill.css({
+		                top: -65,
+		                left: randomX
+		            })   
+		        }
+		        else {
+		            bill.css({ top: currPos2 + yspeed });
+		        }                    
+		    }
+		    setInterval(function(){ ymovement();}, 1000/60 )
+
+		}
 
 
 
 
-    // DROP 1 FUNCTION
+	 }
 
-	function drop(bill, yspeed) {
 
-	    var currPos = bill.offset();
-	    var leftPos = currPos.left;
-	    var topPos = currPos.top;
-	    var pageHeight = $(window).height();
-	    var pageWith = $(window).width();
-	    var randomX = Math.floor(Math.random() * pageWith );
 
-	    console.log(randomX);               
-	   
-	    
-	    var zigzag = function()  {
-
-	        console.log('zigzag');
-	        
-	        bill.transition({ x: leftPos + 60, onComplete: function(){
-	            bill.transition({x: leftPos - 60});
-	            zigzag();
-	            }
-	        });
-	    }
-
-	    zigzag();
-
-	    var ymovement = function() {
-
-	        var currPos2 = bill.offset().top;
-	        if ( currPos2 > pageHeight ) {
-	        
-	            var pageWith = $(window).width();
-	            var randomX = Math.floor(Math.random() * pageWith );
-	        
-	            bill.css({
-	                top: -65,
-	                left: randomX
-	            })   
-	        }
-	        else {
-	            bill.css({ top: currPos2 + yspeed });
-	        }                    
-	    }
-	    setInterval(function(){ ymovement();}, 1000/60 )
-
-	}
 
 
 
